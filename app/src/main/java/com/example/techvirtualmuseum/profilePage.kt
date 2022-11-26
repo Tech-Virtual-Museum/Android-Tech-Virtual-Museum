8eb4b6ca6454f510f24b6d843f2b968ef6299e46
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-11-26 21:23:32 +0000

    funcionando correctamente la obtencion de informacion del usuario

--- app/src/main/java/com/example/techvirtualmuseum/profilePage.kt
+++ app/src/main/java/com/example/techvirtualmuseum/profilePage.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b257a7fc76f54fecbec9298efcd6dd8919d95320a64193aa1ce15920cbfc80ed
-size 2263
+oid sha256:62d748d8ff8bbed98148be9521e9c4e44a159c141c3c9c7500dfeba0cd99808f
+size 2316
