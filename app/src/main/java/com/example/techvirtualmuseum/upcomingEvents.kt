a35b05f0c4ebb758b44f78b8315472ee61772905
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-11-26 22:57:25 +0000

    obtencion de evento de forma manual

--- app/src/main/java/com/example/techvirtualmuseum/upcomingEvents.kt
+++ app/src/main/java/com/example/techvirtualmuseum/upcomingEvents.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81ba42483eac52cc6179410d6a1d9a6564a07ea089265116d534948893362104
-size 1111
+oid sha256:1e0c20c3dedecfa8c1596a22128c3aa6a3652066a12d913353afc596fa1a55cf
+size 1387
