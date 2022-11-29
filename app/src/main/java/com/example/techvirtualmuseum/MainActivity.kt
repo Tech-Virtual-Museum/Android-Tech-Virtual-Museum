e8296ca49a1a4216668061b2ac09a25fb2e30759
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-11-29 19:56:39 +0000

    reestructuracion final pantalla principal a falta del mapa v2

--- app/src/main/java/com/example/techvirtualmuseum/MainActivity.kt
+++ app/src/main/java/com/example/techvirtualmuseum/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2a491be50195a0efb3c06530bcbd5e6e9ee5a9f1f2ec516cf4e9957a2a102bb
-size 3183
+oid sha256:a86e4fba4023693077438b2b7d3876f049f9461ba90e0d7deea52e933c658a2f
+size 3185
