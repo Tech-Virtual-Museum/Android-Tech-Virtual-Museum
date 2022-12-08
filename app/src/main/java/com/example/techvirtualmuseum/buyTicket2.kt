bd024338d91187676c0a6d980a3b5a5d14576058
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-12-08 11:36:07 +0000

    muestra del precio total por numero de entradas

--- app/src/main/java/com/example/techvirtualmuseum/buyTicket2.kt
+++ app/src/main/java/com/example/techvirtualmuseum/buyTicket2.kt
@@ -3,7 +3,6 @@ package com.example.techvirtualmuseum
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,16 +14,34 @@ class buyTicket2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_ticket2)
 
+        //PRECIOS ENTRADAS
+        val ticketJR : Double = 2.50
+        val ticketST : Double = 4.50
+        val ticketAD : Double = 7.00
+        val ticketSN : Double = 4.50
+
         //Variables JUNIOR
         val cantidadJR : EditText = findViewById(R.id.cantidadJunior)
         val n1 : String = cantidadJR.text.toString()
         var valorJR : Int = Integer.parseInt(n1)
+
+        //Inicialiazacion de los campos para mostrar el precio
+        val numTicketJr : EditText = findViewById(R.id.numTicketsJR)
+        val numTicketSt : EditText = findViewById(R.id.numTicketsST)
+        val numTicketAd : EditText = findViewById(R.id.numTicketsAD)
+        val numTicketSn : EditText = findViewById(R.id.numTicketsSN)
 
         //boton con el que aumentamos el numero de entradas
         val aumentarBtnJunior : Button = findViewById(R.id.sumaJunior)
         aumentarBtnJunior.setOnClickListener {
             valorJR += 1
             cantidadJR.setText("$valorJR")
+
+            //aumentacmos precio en el Summary
+            if (valorJR >= 1) {
+                val precioJR = valorJR * ticketJR
+                numTicketJr.setText("$precioJR")
+            }
         }
 
         //boton con el que aumentamos el numero de entradas
@@ -36,6 +53,7 @@ class buyTicket2 : AppCompatActivity() {
             }
         }
 
+
         //Variables STUDENT
         val cantidadST : EditText = findViewById(R.id.cantidadStudent)
         val n2 : String = cantidadST.text.toString()
@@ -46,6 +64,12 @@ class buyTicket2 : AppCompatActivity() {
         aumentarBtnStudent.setOnClickListener {
             valorST += 1
             cantidadST.setText("$valorST")
+
+            //aumentacmos precio en el Summary
+            if (valorST >= 1) {
+                val precioST = valorST * ticketST
+                numTicketSt.setText("$precioST")
+            }
         }
 
         //boton con el que aumentamos el numero de entradas
@@ -57,6 +81,7 @@ class buyTicket2 : AppCompatActivity() {
             }
         }
 
+
         //Variables ADULT
         val cantidadAD : EditText = findViewById(R.id.cantidadAdult)
         val n3 : String = cantidadAD.text.toString()
@@ -66,6 +91,12 @@ class buyTicket2 : AppCompatActivity() {
         aumentarBtnAdult.setOnClickListener {
             valorAD += 1
             cantidadAD.setText("$valorAD")
+
+            //aumentacmos precio en el Summary
+            if (valorAD >= 1) {
+                val precioAD = valorAD * ticketAD
+                numTicketAd.setText("$precioAD")
+            }
         }
 
         val disminuirBtnAdult : Button = findViewById(R.id.restaAdult)
@@ -76,6 +107,7 @@ class buyTicket2 : AppCompatActivity() {
             }
         }
 
+
         //Variables SENIOR
         val cantidadSN : EditText = findViewById(R.id.cantidadSenior)
         val n4 : String = cantidadSN.text.toString()
@@ -85,6 +117,12 @@ class buyTicket2 : AppCompatActivity() {
         aumentarBtnSenior.setOnClickListener {
             valorSN += 1
             cantidadSN.setText("$valorSN")
+
+            //aumentacmos precio en el Summary
+            if (valorSN >= 1) {
+                val precioSN = valorSN * ticketSN
+                numTicketSn.setText("$precioSN")
+            }
         }
 
         val disminuirBtnSenior : Button = findViewById(R.id.restaSenior)
@@ -94,11 +132,6 @@ class buyTicket2 : AppCompatActivity() {
                 cantidadSN.setText("$valorSN")
             }
         }
-
-
-
-
-
 
         //boton que nos llevara a la actvidad anterior
         val backButton : ImageButton = findViewById(R.id.backButton)
