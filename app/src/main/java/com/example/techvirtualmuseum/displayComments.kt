d4e7837e84cac549d4c66c4e03a55db81e73f889
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-12-17 21:52:27 +0000

    pop up de creaion de comentarios funcionales, a falta de que se guarden en firebase

--- app/src/main/java/com/example/techvirtualmuseum/displayComments.kt
+++ app/src/main/java/com/example/techvirtualmuseum/displayComments.kt
@@ -1,26 +1,25 @@
 package com.example.techvirtualmuseum
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.techvirtualmuseum.adapter.AdapterComments
-import com.example.techvirtualmuseum.adapter.AdapterEventos
 import com.example.techvirtualmuseum.modal.commentModal
-import com.example.techvirtualmuseum.modal.dataModal
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.ktx.Firebase
 
 class displayComments : AppCompatActivity() {
 
+    private lateinit var auth : FirebaseAuth
     var comentariosLista: ListView? = null
     var commentModalArrayList: ArrayList<commentModal>? = null
     var database: FirebaseFirestore? = null
@@ -32,6 +31,7 @@ class displayComments : AppCompatActivity() {
         comentariosLista = findViewById<View>(R.id.listItems) as ListView?
         commentModalArrayList = ArrayList()
         database = FirebaseFirestore.getInstance()
+        auth = Firebase.auth
 
         // llamamos al metodo que nos cargaran los datos en la vista
         loadDatainListview()
@@ -43,15 +43,33 @@ class displayComments : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val idUser = auth.currentUser!!.email
+
+
         //cuando hacemos click
         val floatingButton : FloatingActionButton = findViewById(R.id.floatingButton)
         floatingButton.setOnClickListener{
             val inflater = layoutInflater
             val view = inflater.inflate(R.layout.popup_layout, null)
             val builder = AlertDialog.Builder(this@displayComments)
+            val nameComment = view.findViewById<EditText>(R.id.nameComment)
+            val commentComment = view.findViewById<EditText>(R.id.commentComment)
+            val inputName = nameComment.text.toString()
+            val inputComment = commentComment.text.toString()
             builder.setView(view)
             val dialog = builder.create()
             dialog.show()
+
+            with(builder){
+                val saveComments : Button? = dialog.findViewById(R.id.saveComment)
+                saveComments?.setOnClickListener {
+                    val dato = hashMapOf("name" to inputName, "comment" to inputComment)
+                    database!!.collection("pruebaComentarios").document(idUser!!).set(dato)
+                    val myIntent = Intent(this@displayComments, displayComments::class.java)
+                    startActivity(myIntent)
+                    dialog.cancel() //Cierra dialogo.
+                }
+            }
         }
 
 
